631158a41eed84c489bc21f822ce9806410b551f
Elena753269 <elenasm753@gmail.com>
2026-03-10 00:08:12 +0300

    add categories, change api, add skeleton for categories

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,16 +3,15 @@ import axios from "axios";
 const BASE_URL=import.meta.env.VITE_NEWS_BASE_API_URL;
 const API_KEY=import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async (page=1, pageSize=10) => {
+export const getNews = async (page=1, pageSize=10, category='technology') => {
     try {
-        const response = await axios.get(`${BASE_URL}everything`, 
+        const response = await axios.get(`${BASE_URL}top-headlines`, 
         {
             params: {
-                q: 'технологии',
-                language: 'ru',
                 apiKey: API_KEY,
                 pageSize,
-                page     
+                page,
+                category     
             }
         })
         return response.data;
